64c0a6fd898740bcd327efffe69b714d99fdfc19
hmartiins <hmartins224@gmail.com>
2023-12-24 19:12:53 -0300

    feat: add carousel indicators

--- lib/features/screens/product-page/widgets/product_page_carousel.dart
+++ lib/features/screens/product-page/widgets/product_page_carousel.dart
@@ -2,30 +2,70 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:graodegente/common/consts/product.dart';
 
-class ProductPageCarousel extends StatelessWidget {
+class ProductPageCarousel extends StatefulWidget {
   const ProductPageCarousel({super.key});
 
   @override
+  State<ProductPageCarousel> createState() => _ProductPageCarouselState();
+}
+
+class _ProductPageCarouselState extends State<ProductPageCarousel> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: 400.0,
-        viewportFraction: 1,
-      ),
-      items: ProductConsts.productPhotos.map((String photo) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(color: Colors.amber),
-              child: Image.network(
-                photo,
-                fit: BoxFit.cover,
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 400.0,
+            viewportFraction: 1,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _current = index;
+              });
+            },
+          ),
+          carouselController: _controller,
+          items: ProductConsts.productPhotos.map((String photo) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(color: Colors.grey),
+                  child: Image.network(
+                    photo,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            );
+          }).toList(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: ProductConsts.productPhotos.asMap().entries.map((entry) {
+            return GestureDetector(
+              onTap: () => _controller.animateToPage(entry.key),
+              child: Container(
+                width: (MediaQuery.of(context).size.width /
+                        ProductConsts.productPhotos.length) -
+                    16,
+                height: 2,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 19.0,
+                  horizontal: 4.0,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  color: _current == entry.key ? Colors.cyan : Colors.black26,
+                ),
               ),
             );
-          },
-        );
-      }).toList(),
+          }).toList(),
+        ),
+      ],
     );
   }
 }
