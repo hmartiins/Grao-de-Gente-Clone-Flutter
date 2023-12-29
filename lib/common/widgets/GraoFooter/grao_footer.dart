40e8d65849dc2147317e51b1472f574fbe5c188f
hmartiins <hmartins224@gmail.com>
2023-12-29 20:24:33 -0300

    feat: add info footer

--- lib/common/widgets/GraoFooter/grao_footer.dart
+++ lib/common/widgets/GraoFooter/grao_footer.dart
@@ -32,6 +32,46 @@ class GraoFooter extends StatelessWidget {
                   ),
                 ],
               ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 36),
+                child: SizedBox(
+                  width: 80,
+                  child: Image.network(
+                    "https://image.graodegente.app/staticImages/ivokd_GraoVertical.webp",
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 22),
+                child: Text(
+                  "Grão de Gente - LGF Comércio Eletrônico LTDA - CNPJ: 26.384.531/0001-19",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 22, right: 22),
+                child: Text(
+                  "Rod mg 290, Km 73, s/n - Vargem da Forquilha - Jacutinga/MG - CEP: 37.590-000",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 36),
+                child: SizedBox(
+                  width: 96,
+                  child: Image.network(
+                    "https://image.graodegente.app/staticImages/nu3t6_konduza-logo.webp",
+                  ),
+                ),
+              ),
             ],
           ),
         ),
